f277fc88f6283b926a2ed1ee9345980765398432
JaeHeyK <jaeheyk@gmail.com>
2020-06-09 16:19:15 +0900

    Modify stepper motor code for real work

--- stepperMotorTest/stepperMotorTest.ino
+++ stepperMotorTest/stepperMotorTest.ino
@@ -1,18 +1,24 @@
-#include <Stepper.h>      // 스테핑 모터 라이브러리 정의 
+#include <Stepper.h>
 
-int stepsPerRev = 2048; // 한바퀴(360): 2048 , 반 바퀴(180) : 1024
-Stepper stepper (stepsPerRev, 11,9,10,8); // ( IN4,IN2,IN3,IN1) 
+const int steps = 2048;
+Stepper stepper(steps, 11, 9, 10, 8);
+
+boolean isFinished = 1;
 
 void setup() {
+  stepper.setSpeed(15); // 스텝모터 속도
   Serial.begin(9600);
-  stepper.setSpeed(20);   // 스텝모터의 스피드 설정
 }
 
-void loop()
-{
-  
-  stepper.step(stepsPerRev);  // 한 바퀴 회전 명령
-  delay(1000); 
-  stepper.step(-stepsPerRev);  // 반대 방향으로 한 바퀴 회전
-  delay(1000);
+void loop() {
+  // 작동 시작
+  if ( isFinished == 1 ) { // 고양이가 화장실 이용을 마침 
+    // 정면을 기준으로,
+    stepper.step(1195);  // 반시계방향으로 210도 회전
+    delay(1000);
+    stepper.step(-1365);  // 시계방향으로 240도 회전
+    delay(1000);
+    stepper.step(170);  // 반시계방향으로 30도 회전
+    delay(2000);
+  }
 }
