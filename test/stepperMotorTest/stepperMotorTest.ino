82dc3d520154380ae33d3bddbbef7f787a654140
JaeHeyK <jaeheyk@gmail.com>
2020-06-17 18:11:01 +0900

    Update motor rotate angle

--- test/stepperMotorTest/stepperMotorTest.ino
+++ test/stepperMotorTest/stepperMotorTest.ino
@@ -1,24 +1,30 @@
 #include <Stepper.h>
 
-const int steps = 2048;
+#define steps 2048
 Stepper stepper(steps, 11, 9, 10, 8);
 
-boolean isFinished = 1;
+bool isFinished = 1;
 
 void setup() {
-  stepper.setSpeed(15); // 스텝모터 속도
+  stepper.setSpeed(10); // 스텝모터 속도
+
   Serial.begin(9600);
+ 
+}
+
+void motor ()
+{
+  // 정면을 기준으로,
+  stepper.step(2390);  // 반시계방향으로 210도 회전
+  delay(1000);
+  stepper.step(-2730);  // 시계방향으로 240도 회전
+  delay(1000);
+  stepper.step(340);  // 반시계방향으로 30도 회전
+  delay(2000);
+  
 }
 
 void loop() {
-  // 작동 시작
-  if ( isFinished == 1 ) { // 고양이가 화장실 이용을 마침 
-    // 정면을 기준으로,
-    stepper.step(1195);  // 반시계방향으로 210도 회전
-    delay(1000);
-    stepper.step(-1365);  // 시계방향으로 240도 회전
-    delay(1000);
-    stepper.step(170);  // 반시계방향으로 30도 회전
-    delay(2000);
-  }
+  if ( isFinished == 1 )  // 고양이가 화장실 이용을 마침
+    motor();
 }
